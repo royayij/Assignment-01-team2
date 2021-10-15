87cfc40a4933f2adfe1dea58741109d76aef77d8
Roya <royayij>
2021-10-15 12:49:39 +0200

    add new version of visualization

--- visualization-cp-gcp/app.py
+++ visualization-cp-gcp/app.py
@@ -1,11 +1,7 @@
-import os
-
-import pandas as pd
-import requests
-from flask import Flask, json
+from flask import Flask
 
 from flask import jsonify
-from resources import model_trainer
+from resources import figure_maker
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -13,12 +9,12 @@ app.config["DEBUG"] = True
 
 @app.route('/vizualization-cp/figures', methods=['POST'])
 def make_figure():
-    db_api = os.environ['PREDICTIONDB_API']
-    # Make a GET request to training db service to retrieve the training data/features.
-    r = requests.get(db_api)
-    j = r.json()
-    df = pd.DataFrame.from_dict(j)
-    resp = figure_maker.make_fig(df)
+    # db_api = os.environ['PREDICTIONDB_API']
+    # # Make a GET request to training db service to retrieve the training data/features.
+    # r = requests.get(db_api)
+    # j = r.json()
+    # df = pd.DataFrame.from_dict(j)
+    resp = figure_maker.acc_fig()
     return resp
 
 
--- visualization-cp-gcp/resources/figure_maker.py
+++ visualization-cp-gcp/resources/figure_maker.py
@@ -5,29 +5,47 @@ import time
 from google.cloud import storage
 import os
 from flask import jsonify
+import numpy as np
 
-def make_fig(y):
-    fig_verify = plt.figure(figsize=(100, 50))
-    plt.plot(y[0], color="blue")
-    plt.plot(y[1], color="green")
-    plt.title('prediction')
-    plt.ylabel('value')
-    plt.xlabel('row')
-    plt.legend(['predicted', 'actual data'], loc='upper left')
+
+def acc_fig():
+    # fig_verify = plt.figure(figsize=(100, 50))
+    # plt.plot(y[0], color="blue")
+    # plt.plot(y[1], color="green")
+    # plt.title('prediction')
+    # plt.ylabel('value')
+    # plt.xlabel('row')
+    # plt.legend(['predicted', 'actual data'], loc='upper left')
     now = time.time()
+    project_id = os.environ['PROJECT_ID']
     figure_repo = os.environ['FIGURE_REPO']
-    if figure_repo:
-        # Save the model localy
-        fig_verify.savefig(f"model_verify_{now}.png")
-        # Save to GCS as model.h5
+    history_repo = os.environ['HISTORY_REPO']
+    fig_acc = plt.figure(figsize=(10, 10))
+    if history_repo:
         client = storage.Client(project=project_id)
-        bucket = client.get_bucket(figure_repo)
-        blob = bucket.blob(f"model_verify_{now}.png")
-        # Upload the locally saved model
-        blob.upload_from_filename(f"model_verify_{now}.png")
-        # Clean up
-        os.remove(f"model_verify_{now}.png")
-        return jsonify({'message':"Saved the figure to GCP bucket : " + model_repo}), 200
+        history_bucket = client.get_bucket(history_repo)
+        history_blob = history_bucket.blob('my_history.npy')
+        history_blob.download_to_filename('my_history.npy')
+        history = np.load('my_history.npy', allow_pickle='TRUE')
+        plt.plot(history['accuracy'])
+        plt.plot(history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        # Save the model localy
+        if figure_repo:
+            fig_acc.savefig(f"model_acc_{now}.png")
+            # Save to GCS
+            fig_bucket = client.get_bucket(figure_repo)
+            fig_blob = fig_bucket.blob(f"model_verify_{now}.png")
+            # Upload the locally saved model
+            fig_blob.upload_from_filename(f"model_acc_{now}.png")
+            # Clean up
+            os.remove(f"model_acc_{now}.png")
+            return jsonify({'message': "Saved the accuracy figure to GCP bucket : " + figure_repo}), 200
+        else:
+            fig_acc.savefig(f"model_acc_{now}.png")
+            return jsonify({'message': 'The Accuracy figure was saved locally.'}), 200
     else:
-        fig_verify.savefig(f"model_verify_{now}.png")
-        return jsonify({'message': 'The model was saved locally.'}), 200
+        return jsonify({'message': 'The history repo not found.'}), 404
